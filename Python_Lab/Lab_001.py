fe1e500d87d812d3ffa635756639521f1dde686d
Bhumika-jain07 <bhumika44jain@gmail.com>
2024-07-10 14:56:36 +0530

    Calculation + if Else condition

--- Python_Lab/Lab_001.py
+++ Python_Lab/Lab_001.py
@@ -1,2 +1,21 @@
-print("hello")
-print("hello")
+num = float(input("Enter the number \n"))
+
+square = num ** 2
+cube = num ** 3
+
+print(f"The square of {num} is {square} and the cube is {cube}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
