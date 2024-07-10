3273d68aa1c02e2a80d953c4ab2f5f2ce96dd423
Bhumika-jain07 <bhumika44jain@gmail.com>
2024-07-10 18:55:43 +0530

    Multiple Loop (If and Else)

--- Python_Lab/Lab_001.py
+++ Python_Lab/Lab_001.py
@@ -1,4 +1,34 @@
+# Write a program that calculates and displays the letter grade for a given numerical score (e.g., A, B, C, D, or F) based on the following grading scale:
+# input- score - 89
+# output- B
+# A: 90-100
+# B: 80-89
+# C: 70-79
+# D: 60-69
+# F: 0-59
 
+
+
+num = int(input("Enter a number: " ))
+
+
+if num>=90 and num <=100:
+    print("A")
+
+elif num>=80 and num <=89:
+    print("B")
+
+elif num>=70 and num <=79:
+    print("C")
+
+elif num>=60 and num <=69:
+    print("D")
+
+elif num>=0 and num <=59:
+    print("F")
+
+else:
+    print("Invalid number")
 
 
 
