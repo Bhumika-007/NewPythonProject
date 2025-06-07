3647d07d116c579bb164372c7d111db7c520f5b7
Bhumika-jain07 <bhumika44jain@gmail.com>
2025-06-07 13:22:18 +0530

    Practice

---
+++ Python_Lab/Lab_002.py
@@ -0,0 +1,25 @@
+# print Hello string.
+
+print ("hello");
+
+# take 2 numbers from user and add them.
+
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter the second number"))
+add = num1+num2
+print(add)
+print(type(add))
+
+
+
+# take 2 value from user and concatenate them.
+
+value1 = "Come"
+value2 = "go"
+result = value1+value2
+print(result)
+
+
+
+
+
