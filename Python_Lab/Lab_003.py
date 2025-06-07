9f0728d81c03de7fbbd2fc53fa6f143abc5f577b
Bhumika-jain07 <bhumika44jain@gmail.com>
2025-06-07 20:00:51 +0530

    Lab_003

---
+++ Python_Lab/Lab_003.py
@@ -0,0 +1,24 @@
+shopping_list = ["milk", "butter", "bread", "poha"]
+
+print(shopping_list)
+shopping_list.append("curd") #append is for add item in end
+print(shopping_list)
+print(shopping_list[2])
+print(shopping_list[-1])
+print(shopping_list[-4])
+
+shopping_list.insert(4 ,"jam") #insert is for add item in middle of array
+print(shopping_list)
+
+
+#------------------------------------------------------------------------------------------------------
+
+#SET -- it is a collection of unique items into the curly brackets.
+
+my_set = {1,2,3,4,5,6,6,6,6,6,5,5,6,6,2,}
+print(my_set)
+
+
+
+
+
