492de935a7b1f86983be0d86d49556fc7556146d
Bhumika-jain07 <bhumika44jain@gmail.com>
2025-06-12 11:45:17 +0530

    leap year

--- Python_Lab/leap_year.py
+++ Python_Lab/leap_year.py
@@ -15,3 +15,21 @@ else:
     print("No idea")
 
 
+# ---------------------------------------------------------------------------------------------------------------------
+
+    # leap year with function
+    def leap_year(year):
+        if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+            return True
+        else:
+            return False
+
+
+    year = int(input("Enter the year"))
+
+    if leap_year(year):
+        print("leap year")
+    else:
+        print("not")
+
+
